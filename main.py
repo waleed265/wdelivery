5b94ed02b9678899b16cec4aaeb366651bf2b01b
waleed265 <muhammadwaleed.iqbal@abacuscambridge.com>
2020-12-23 10:30:09 +0000

    adding changes

--- main.py
+++ main.py
@@ -19,10 +19,5 @@ def waleed():
     val = {"value": "waleed"}
     return jsonify(val)
 
-@app.route('/abc')
-def abc():
-    val = {"value": "abc"}
-    return jsonify(val)
-
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
