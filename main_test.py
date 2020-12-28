0d1ccb813cb77b52bc1b7557288cfdda32af15c5
waleed265 <muhammadwaleed.iqbal@abacuscambridge.com>
2020-12-28 14:13:53 +0000

    making testing changes

--- main_test.py
+++ main_test.py
@@ -1,10 +1,29 @@
 import main
+import unittest
 
+class NamesTesCase(unittest.TestCase):
+    def test_index(self):
+        
+        main.app.testing = True 
+        client = main.app.test_client()
 
-def test_index():
-    main.app.testing = True
-    client = main.app.test_client()
+        r = client.get('/')
+        #assert r.status_code == 200, "status code should be '200'"
+        assert 'Hello' in r.data.decode('utf-8')
 
-    r = client.get('/')
-    assert r.status_code == 400, "status code should be '400'"
-    assert 'Hello World' in r.data.decode('utf-8')
+        #self.assertTrue('teaher'.islower())
+
+    def test_index1(self):
+        print("Hello 1234")
+        
+        main.app.testing = True 
+        client = main.app.test_client()
+
+        r = client.get('/')
+        assert r.status_code == 400, "status code should be '400'"
+        #assert 'Hello' in r.data.decode('utf-8')
+
+        #self.assertTrue('teaher'.islower())
+
+if __name__=='__main__':
+    unittest.main()
